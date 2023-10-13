06b5fafdf2ce2c1693ebe5207e0aa2c0308fd73a
Emanoel Sabino <105612311+emanoelsabino@users.noreply.github.com>
2023-10-13 09:35:47 -0300

    portfolio v2.0
    ajustes das imagens e criação de efeito nas img sobre mim

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 const btnMobile = document.getElementById('btn-mobile');
+const elementos=document.querySelectorAll('[data-anima]');
+const animacaoClass='animacao';
+
+function animaRolagem(){
+    const topoPagina=window.pageYOffset+((window.innerHeight)*0.9);
+    elementos.forEach(function(elemento){
+        if(topoPagina > elemento.offsetTop){
+            elemento.classList.add(animacaoClass);
+        }else{
+            elemento.classList.remove(animacaoClass);
+        }
+    });
+}
+
+if(elementos.length){
+    window.addEventListener('scroll', function(){
+        animaRolagem();
+    })
+}
 
 function toggleMenu() {
     const nav = document.getElementById('nav');
